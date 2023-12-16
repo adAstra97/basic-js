beeb38254fcbf07c84ffe95e846dbabec4ffc89f
adAstra97 <131522998+adAstra97@users.noreply.github.com>
2023-12-16 23:45:19 +0300

    feat: add last task about vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,11 +20,75 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-
+  constructor(isDirectMachine = true) {
+    this.isDirectMachine = isDirectMachine;
   }
-  decrypt() {
-    
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let keyIdx = 0;
+    let result = '';
+
+    for (let i = 0; i < message.length; i++) {
+      let charCodeLetter = message.charCodeAt(i);
+
+      if (charCodeLetter >= 65 && charCodeLetter <= 90) {
+        let meaningNum = charCodeLetter - 65;
+        let meaningKey = key.charCodeAt(keyIdx % key.length) - 65;
+        let module = (meaningNum + meaningKey) % 26;
+
+        result += String.fromCharCode(module + 65);
+
+        keyIdx++;
+      } else {
+        result += message[i];
+      }
+
+    }
+
+    return this.isDirectMachine ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    let keyIdx = 0;
+    let result = '';
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+        let charCodeLetter = encryptedMessage.charCodeAt(i);
+
+        if (charCodeLetter >= 65 && charCodeLetter <= 90) {
+            let meaningNum = charCodeLetter - 65;
+            let meaningKey = key.charCodeAt(keyIdx % key.length) - 65;
+            let numForModule = meaningNum - meaningKey;
+            let module;
+
+            if (numForModule > 0) {
+                module = numForModule % 26;
+            } else {
+                module = (numForModule + 26) % 26;
+            }
+
+            result += String.fromCharCode(module + 65);
+            keyIdx++;
+        } else {
+            result += encryptedMessage[i];
+        }
+    }
+
+    return this.isDirectMachine ? result : result.split('').reverse().join('');
   }
 }
 
